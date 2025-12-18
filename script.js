5b0e57beb4593261a242ee6b11127dfe7c33d260
mannT21 <desale.manisha21@gmail.com>
2025-12-18 18:06:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const items = document.querySelectorAll(".reveal");
+
+const observer = new IntersectionObserver(
+  entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("active");
+      }
+    });
+  },
+  { threshold: 0.15 }
+);
+
+items.forEach(el => observer.observe(el));
